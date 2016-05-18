862d41edeb45636ad750b55eb7f65bf87cb75aba
joaquim <joaquim.perez@he-arc.ch>
2016-05-18 16:04:59 +0200

    v0.0.1

---
+++ lol.py
@@ -0,0 +1,28 @@
+﻿
+nbTours=5
+nbMotsApparents=3
+histoire=""
+
+for i in range(nbTours):
+	
+	print("\n"*10);
+	
+	print("tour " + str(i+1) + "/" + str(nbTours), end=" : ")
+	if(i==0):
+		print("Commencez l'histoire")
+	elif(i==nbTours-1):
+		print("Finissez l'histoire")
+	else:
+		print("Continuez l'histoire")
+	
+	mots = histoire.split();
+	for mot in mots[-nbMotsApparents:]:
+		print(mot,end=' ')
+	
+	histoire += input()
+	
+	if(histoire[-1]!=' '):
+		histoire += ' '
+		
+
+print("\n\nHistoire : " + histoire)
