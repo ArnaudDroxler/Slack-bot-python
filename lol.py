c96a6db49c6404353825580abe2594eadb62c2ce
joaquim <joaquim.perez@he-arc.ch>
2016-05-18 16:44:18 +0200

    configuration de la partie

--- lol.py
+++ lol.py
@@ -1,13 +1,37 @@
 ﻿
-nbTours=5
-nbMotsApparents=3
+nbToursParJoueur=0
+nbMotsApparents=0
 histoire=""
+listeJoueur=[]
+ptr=0
+
+while(len(listeJoueur) < 1):
+	print("Entrez le pseudo des joueurs, séparés par des espaces")
+	listeJoueur = input().split()
+
+
+def getNumberInput(question):
+	variable=0
+	while(variable < 1):
+		print(question)
+		try:
+			variable = int(input())
+		except:
+			variable = 0
+	return variable		
+			
+nbToursParJoueur = getNumberInput("Combien de tours par joueur ?")
+nbMotsApparents = getNumberInput("Combien de mots apparents ?")
+	
+nbTours = nbToursParJoueur*len(listeJoueur)
 
 for i in range(nbTours):
 	
 	print("\n"*10);
 	
-	print("tour " + str(i+1) + "/" + str(nbTours), end=" : ")
+	print(listeJoueur[ptr] + " - tour " + str(i+1) + "/" + str(nbTours), end=" : ")
+	ptr = (ptr+1)%len(listeJoueur)
+	
 	if(i==0):
 		print("Commencez l'histoire")
 	elif(i==nbTours-1):
