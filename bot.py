fa51f464a9ef5a9ff5f130107ac5029dc3b19fb4
joaquim <joaquim.perez@he-arc.ch>
2016-05-25 18:59:18 +0200

    le bot répète ce qu'on lui dit

--- bot.py
+++ bot.py
@@ -2,19 +2,24 @@ import time
 
 from slackclient import SlackClient
 
-TOKEN="xoxb-38243441969-g87a3Ok5QZ6vGab5HvuMrF3i"
+TOKEN="xoxb-45725674864-VkW4jUC0mwxEvEUipTOTextD"
 
 bot = SlackClient(TOKEN)
+
 if bot.rtm_connect():
-    cpt = 0
-    try:
-        while True:
-            messages = bot.rtm_read()
-            if messages:
-                print(str(cpt) + " ::: " + str(messages[0]['type']))
-                cpt = cpt+1
-                #bot.rtm_send_message([channel = messages[0]['channel'], message = messages[0]['text']])
-            else:
-                time.sleep(1)
-    except KeyboardInterrupt:
-        pass
+	try:
+		while True:
+			messages = bot.rtm_read()
+			if messages:
+				for message in messages :
+					#print(message)
+					if "type" in message and message['type'] == 'message':
+						if message['user'] != 'U1BMBKURE': # id du bot
+							print(message['user'] + " dit " + message['text'])
+							bot.rtm_send_message(message['channel'], message['text'])
+			else:
+				time.sleep(1)
+	except KeyboardInterrupt:
+		pass
+else:
+	print("connexion échouée")
