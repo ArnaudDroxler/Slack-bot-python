7aa7990aff29f3298e77c128c9daf41f795f18dd
joaquim <joaquim.perez@he-arc.ch>
2016-06-02 17:12:55 +0200

    fonction validate qui contrôle la syntaxe de lancement d'une partie et si les joueurs donnés existent dans la team

--- faqfinoubot/bot.py
+++ faqfinoubot/bot.py
@@ -13,7 +13,9 @@ g_indice_joueur=0
 g_mots=[]
 g_tour=0
 g_bot = SlackClient(TOKEN)
+g_slack_users_list=[]
 
+			
 def send(joueur, message) :
 	reponse = g_bot.api_call("chat.postMessage", as_user=True, channel=joueur, text=message)
 	print(reponse)
@@ -23,7 +25,26 @@ def sendToAll(listeJoueurs, message) :
 	for joueur in listeJoueurs:
 		send(joueur, message)
 
-
+def validate(user, message):
+	global g_joueurs
+	
+	print(">>>>>>>> validation du message : " + message)
+	mots = message.split()
+	print(">>>>>>>>>> mots : " + str(mots))
+	if(mots[0]=="start"):
+		g_joueurs = [user]
+		for mot in mots[1:]:
+			joueur = mot[2:-1]
+			if(joueur in g_slack_users_list):
+				g_joueurs.append(joueur)
+			else:
+				return {"ok":False, "error": mot+" n'est pas connu"}
+		return {"ok":True}
+	else:
+		return {"ok":False, "error":"Pour commencez une partie, écrivez : start @joueur1 @joueur2 @joueur3 etc..."}
+		
+		
+		
 def process(user, message):
 	global g_etat, g_g_tours_par_joueur, g_nb_mots_apparents, g_joueurs, g_nb_g_tours, g_indice_joueur, g_mots, g_tour
 	
@@ -31,18 +52,18 @@ def process(user, message):
 	print(">>>> user : " + user + "  -- message : " + message + " -- etat : " + g_etat)
 
 	if(g_etat=="CONNECTE"):
-		print(g_etat)
-		print(user + " dit " + message)
-		g_joueurs = [user]
-		for joueur in message.split():
-			g_joueurs.append(joueur[2:-1])
-
-		print(g_joueurs)
-		sendToAll(g_joueurs, "Une partie de cadavre exquis est lancée, restez attentifs")
-		g_etat="DEBUT_JEU"
+		print(">>>>>> etat : " + g_etat)
+		
+		validation = validate(user, message);
+		print(">>>>>>>> validation : " + str(validation))
+		if(validation["ok"]):
+			sendToAll(g_joueurs, "Une partie de cadavre exquis est lancée, restez attentifs")
+			g_etat="DEBUT_JEU"
+		else:
+			send(user, validation["error"])
 
 	if(g_etat=="DEBUT_JEU"):
-		print(g_etat)
+		print(">>>>>> etat : " + g_etat)
 		g_g_tours_par_joueur = 2
 		g_nb_mots_apparents = 5
 		g_nb_g_tours = g_g_tours_par_joueur*len(g_joueurs)
@@ -53,7 +74,7 @@ def process(user, message):
 		g_etat="JEU_EN_COURS"
 
 	if(g_etat=="JEU_EN_COURS"):
-		print(g_etat)
+		print(">>>>>> etat : " + g_etat)
 
 		g_tour = g_tour+1
 
@@ -80,7 +101,7 @@ def process(user, message):
 		g_indice_joueur = (g_indice_joueur+1)%len(g_joueurs)
 
 	if(g_etat=="FIN_JEU"):
-		print(g_etat)
+		print(">>>>>> etat : " + g_etat)
 		his=''
 		for mot in g_mots:
 			his += mot + ' '
@@ -88,12 +109,15 @@ def process(user, message):
 
 
 def main():
-	global g_etat
+	global g_etat, g_slack_users_list
 	
 	botInfos = g_bot.api_call("rtm.start", token=TOKEN)
 	if botInfos['ok']:
 		bot_id = botInfos['self']['id']
 		print(bot_id)
+		for member in botInfos['users']:
+			g_slack_users_list.append(member['id'])
+		
 	if g_bot.rtm_connect():
 		g_etat="CONNECTE"
 		try:
@@ -108,7 +132,7 @@ def main():
 								process(message['user'], message['text'])
 
 				else:
-					time.sleep(1)
+					time.sleep(0.01)
 		except KeyboardInterrupt:
 			pass
 	else:
